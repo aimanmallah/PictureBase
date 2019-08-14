c80e7b7d1df57abf8161d72468bf0d6b86610e23
Aiman Mallah <aimanmallah@gmail.com>
2019-08-14 19:03:50 +0100

    finishing off picbase API fun

--- src/Components/ImageIndex.js
+++ src/Components/ImageIndex.js
@@ -8,7 +8,7 @@ class ImageIndex extends React.Component {
         super()
         this.state = {
             data : [],
-            search: ''
+            search: 'nature'
         }
     }
 
@@ -31,7 +31,6 @@ class ImageIndex extends React.Component {
       }
 
     handleSubmit = () => {
-        console.log('this.state.search', this.state.search)
         axios.get('https://api.unsplash.com/search/photos/', {
             params: {
                 client_id: '817944c4f33c3d0024d8e30a6376e884a1dd5f75f89649a44c3c0b56c4f9267f',
@@ -43,14 +42,23 @@ class ImageIndex extends React.Component {
         .then(res => this.setState({data : res.data}))
     }
 
+    handleButton = (e) => {
+        axios.get('https://api.unsplash.com/search/photos/', {
+            params: {
+                client_id: '817944c4f33c3d0024d8e30a6376e884a1dd5f75f89649a44c3c0b56c4f9267f',
+                query: e.target.value,
+                per_page: 99,
+                orientation: 'landscape'
+            }
+        })
+        .then(res => this.setState({data : res.data}))
+    }
+
 
     render(){
         if(!this.state.data.results) return null
-        console.log('state', this.state)
         return(
             <div className="body">
-                <div clasName="header">
-                </div>
                 <section className="wrapper">
 
                     <div className="header">
@@ -63,24 +71,57 @@ class ImageIndex extends React.Component {
                                 type="text"
                                 placeholder="Search free high-resolution photos"
                                 onChange={this.handleChange}
-                                value={this.state.search}
                             >
                             </input>
                             <button 
                                 type="button" 
                                 onClick={this.handleSubmit}
                             >
-                                Search
+                                search.
                             </button>
                         </div>
                     </div>
 
                     <div className="popular">
-                        <button>Nature</button>
-                        <button>Wallpapers</button>
-                        <button>Business</button>
-                        <button>Travel</button>
-                        <button>Fashion</button>
+                        <button
+                            type="button"
+                            value="nature"
+                            onClick={this.handleButton}
+                        >
+                            nature.
+                        </button>
+
+                        <button
+                            type="button"
+                            value="wallpaper"
+                            onClick={this.handleButton}
+                        >
+                            wallpapers.
+                        </button>
+
+                        <button
+                            type="button"
+                            value="business"
+                            onClick={this.handleButton}
+                        >
+                            business.
+                        </button>
+
+                        <button
+                            type="button"
+                            value="travel"
+                            onClick={this.handleButton}
+                        >
+                            travel.
+                        </button>
+
+                        <button
+                            type="button"
+                            value="fashion"
+                            onClick={this.handleButton}
+                        >
+                            fashion.
+                        </button>
                     </div>
 
                     <div className="index">
@@ -92,7 +133,7 @@ class ImageIndex extends React.Component {
                     </div>
 
                     <div className="footer">
-                        <h1>FOOTER</h1>
+                        <h4>	&reg; Developed by Aiman</h4>
                     </div>
 
                 </section>
