04ace07a11093010774cf333c5c121c0ce167ea9
Aiman Mallah <aimanmallah@gmail.com>
2019-08-14 19:15:53 +0100

    Updates

---
+++ precache-manifest.5c4dacddf0cc2b569c1d1c7ea635dcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c763bc49daeebcf1ceaea6795821a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b43e72ab78092c8cc1c0",
+    "url": "/static/css/main.59f7b876.chunk.css"
+  },
+  {
+    "revision": "919a32d3dd1fcc926618",
+    "url": "/static/js/2.7f82bd93.chunk.js"
+  },
+  {
+    "revision": "b43e72ab78092c8cc1c0",
+    "url": "/static/js/main.a0d6b874.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5e52a172b785379475807b1c10bb661d",
+    "url": "/static/media/PictureBase.5e52a172.jpg"
+  }
+]);
